e20f8bbc711129cd86cbbdefdaa7192f19c69060
Janny Rakotoarimanana <44327690+jmRakoto@users.noreply.github.com>
2022-02-17 23:44:51 +0300

    .

--- controllers/CarControllers.js
+++ controllers/CarControllers.js
@@ -3,16 +3,17 @@ const Car = require("../entity/Car");
 class CarController {
   static getAllCar = async (req, res) => {
     try {
+      
       // If connected user get car list with comments
       if (req.decoded) {
         const result = await Car.find().populate("comments");
         return res.status(200).send(result);
       } else {
         // If user isn't connected remove comments
-
         const result = await Car.find().select("-comments");
         return res.status(200).send(result);
       }
+      
     } catch (err) {
       return res.status(500).send(err);
     }
@@ -20,6 +21,7 @@ class CarController {
 
   static newCar = async (req, res) => {
     try {
+      
       const { immatriculation } = req.body;
       const ifExistCar = await Car.findOne({ immatriculation });
 
@@ -31,13 +33,15 @@ class CarController {
       const result = await insertCar.save();
 
       return res.status(200).send(result);
+      
     } catch (err) {
+      
       if (err.name == "ValidationError") {
         const key = Object.keys(err.errors);
         return res.status(400).json({ error: `${key} is required` });
       }
-
       return res.status(500).send(err);
+      
     }
   };
 }
